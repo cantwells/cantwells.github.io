145be8a1b13f239877522ac2ace1a88fa1f23e3c
cantwell <rjazantsev@gmail.com>
2020-05-26 17:36:47 +0300

    show/hide sub-menu

--- js/main.js
+++ js/main.js
@@ -2,7 +2,6 @@
 const leftMenu = document.querySelector('.left-menu'),
     hamburger = document.querySelector('.hamburger');
 
-
 //Вывод меню
 hamburger.addEventListener('click', event => {
     leftMenu.classList.toggle('openMenu');
@@ -16,5 +15,16 @@ document.addEventListener('click', event => {
     if (!target.closest('.left-menu')) {
         leftMenu.classList.remove('openMenu');
         hamburger.classList.remove('open');
+    }
+})
+
+//Показывать подпункты
+leftMenu.addEventListener('click', event => {
+    const target = event.target;
+    const dropdown = target.closest('.dropdown');
+    if (dropdown) {
+        dropdown.classList.toggle('active');
+        leftMenu.classList.add('openMenu');
+        hamburger.classList.add('open');
     }
 })
