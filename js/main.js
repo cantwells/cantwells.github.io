36d308506aafd032d32966bbb77d4fb87e3396da
cantwell <rjazantsev@gmail.com>
2020-05-27 17:10:02 +0300

    webinar day 2

--- js/main.js
+++ js/main.js
@@ -1,16 +1,60 @@
 const API_KEY = "fa9de1bb626c74440bbf2532a0d596a0";
-
-class DBConnect {
-    getData(url) {
-        // fetch( url )
-    }
-}
-
 //Получение элементов
 const leftMenu = document.querySelector('.left-menu'),
     hamburger = document.querySelector('.hamburger'),
     tvShowsList = document.querySelector('.tv-shows__list'),
     modal = document.querySelector('.modal');
+
+class DBConnect {
+    getData = async(url) => {
+        const res = await fetch(url);
+
+        if (res.ok) {
+            return res.json();
+        } else {
+            throw new Error(`Не удалось получить данные с ${url}`);
+        }
+    }
+
+    getTestData = () => {
+        return this.getData('./test.json');
+    }
+}
+
+const renderCard = response => {
+    const PATH = "https://image.tmdb.org/t/p/w185_and_h278_bestv2/";
+    const data = response.results;
+    tvShowsList.textContent = '';
+
+    data.forEach((movie) => {
+        const {
+            name: title,
+            poster_path: poster,
+            backdrop_path: backdrop,
+            vote_average: vote,
+        } = movie;
+
+        const posterImg = poster ? PATH + poster : '../img/no-poster.jpg';
+        const backdropImg = '';
+        const voteImg = '';
+
+        const card = document.createElement('li');
+        card.classList.add('tv-shows__item');
+        card.innerHTML = `
+        <a href="#" class="tv-card">
+            <span class="tv-card__vote">${vote}</span>
+            <img class="tv-card__img" src="${posterImg}" 
+            data-backdrop="${PATH + backdrop}" 
+            alt="${title}">
+            <h4 class="tv-card__head">${title}</h4>
+        </a>`;
+        tvShowsList.append(card);
+    });
+
+}
+
+new DBConnect().getTestData().then(renderCard).catch(e => console.log(e.message));
+
 
 //Вывод меню
 hamburger.addEventListener('click', event => {
